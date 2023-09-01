b8853c25750a9962d21bebc1ba8a817b4ae48d1d
ProfPriyen <profpriyen@gmail.com>
2023-09-01 15:57:09 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       <Route path="/" element={<Authentication /> }/>
         <Route path="/components/*" element={<Components/>} />
         {/* Other routes */}
+        {/* Git changed */}
       </Routes>
      </Router>
      
